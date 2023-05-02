8a399a6548f81661e3c66cb568a1616f8ede4ebe
haotian <37172891+haz060@users.noreply.github.com>
2023-05-02 13:56:04 -0700

    Create canonical.py
    mapping from taskA and taskB subsection headers to canonical subsection headers

---
+++ taskB/canonical.py
@@ -0,0 +1,61 @@
+mapping_taskB_to_canonical = {
+  # this dictionary stores the mapping from taskB subsection headers to canonical taskB subsection headers
+  # key: taskB subsection headers
+  # value: canonical taskB subsection headers
+  
+    'CHIEF COMPLAINT' : 'CHIEF COMPLAINT',
+    'RESULTS' : 'RESULTS',
+    'REVIEW OF SYSTEMS' : 'REVIEW OF SYSTEMS',
+    'HISTORY OF PRESENT ILLNESS' : 'HISTORY OF PRESENT ILLNESS',
+    'PHYSICAL EXAM' : 'PHYSICAL EXAM',
+    'PLAN' : 'ASSESSMENT AND PLAN',
+    'INSTRUCTIONS' : 'INSTRUCTIONS',
+    'ASSESSMENT AND PLAN' : 'ASSESSMENT AND PLAN',
+    'ASSESSMENT' : 'ASSESSMENT AND PLAN',
+    'SOCIAL HISTORY' : 'FAMILY AND SOCIAL HISTORY',
+    'VITALS' : 'VITALS',
+    'MEDICATIONS' : 'MEDICATIONS',
+    'MEDICAL HISTORY' : 'MEDICAL HISTORY',
+    'PHYSICAL EXAMINATION' : 'PHYSICAL EXAM',
+    'FAMILY HISTORY' : 'FAMILY AND SOCIAL HISTORY',
+    'PAST HISTORY' : 'HISTORY OF PRESENT ILLNESS', 
+    'CURRENT MEDICATIONS' : 'MEDICATIONS',
+    'SURGICAL HISTORY' : 'SURGICAL HISTORY',
+    'ALLERGIES' : 'ALLERGIES',
+    'CC:' : 'CHIEF COMPLAINT',
+    'EXAM' : 'PHYSICAL EXAM',
+    'IMPRESSION' : 'ASSESSMENT AND PLAN',
+    'VITALS REVIEWED' : 'VITALS',
+    'PROCEDURE' : 'ASSESSMENT AND PLAN',
+    'BIRTH HISTORY' : 'FAMILY AND SOCIAL HISTORY'
+}
+
+mapping_taskA_to_canonical = {
+  # this dictionary stores the mapping from taskA subsection headers to canonical taskB subsection headers
+  # key: taskA subsection headers
+  # value: canonical taskB subsection headers
+  
+    'FAM/SOCHX' : 'FAMILY AND SOCIAL HISTORY',
+    'GENHX' : 'HISTORY OF PRESENT ILLNESS',
+    'PASTMEDICALHX' : 'MEDICAL HISTORY',
+    'PASTSURGICAL' : 'SURGICAL HISTORY',
+    'MEDICATIONS' : 'MEDICATIONS',
+    'CC' : 'CHIEF COMPLAINT',
+    'ROS' : 'REVIEW OF SYSTEMS',
+    'ALLERGY' : 'ALLERGIES',
+    'ASSESSMENT' : 'ASSESSMENT AND PLAN',
+    'EDCOURSE' : None,
+    'EXAM' : 'PHYSICAL EXAM',
+    'PLAN' : 'ASSESSMENT AND PLAN',
+    'DISPOSITION' : None,
+    'DIAGNOSIS' : 'RESULTS',
+    'OTHER_HISTORY' : 'HISTORY OF PRESENT ILLNESS',
+    'IMMUNIZATIONS' : None,
+    'LABS' : 'RESULTS',
+    'GYNHX' : 'HISTORY OF PRESENT ILLNESS',
+    'PROCEDURES' : 'ASSESSMENT AND PLAN',
+    'IMAGING' : 'RESULTS'
+}
+
+
+
