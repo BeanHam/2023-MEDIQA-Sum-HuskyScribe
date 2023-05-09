487d475ac013a288fead7e399acd6e9575c49d28
BeanHam <binhan96816@gmail.com>
2023-05-09 16:22:07 -0700

    taskC_run_1

--- taskC/decode_taskC_run1.sh
+++ taskC/decode_taskC_run1.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python taskC-pipeline.py --data taskB_testset4participants_inputConversations.csv
+CUDA_VISIBLE_DEVICES=0 python taskC-pipeline.py --data taskC_testset4participants_inputConversation.csv
